ca8f477151271ac846f4402ae723e1e098c04095
Oscar Soto <oscar.e.soto.g.ing@gmail.com>
2026-06-19 12:29:34 -0600

    Alejadron - Add session persisten for logged-in users

--- frontend/js/menu.js
+++ frontend/js/menu.js
@@ -8,6 +8,18 @@ fetch("menu.html")
         const toggleBtn = document.getElementById("toggle-btn");
         const contenido = document.querySelector(".contenido");
 
+        const logoutBtn = document.getElementById("logoutBtn");
+
+        if (logoutBtn) {
+            logoutBtn.addEventListener("click", function(e) {
+                e.preventDefault();
+
+        localStorage.removeItem("sesionActiva");
+
+        window.location.href = "inicio_sesion.html";
+    });
+}
+
         toggleBtn.addEventListener("click", () => {
 
             sidebar.classList.toggle("closed");
