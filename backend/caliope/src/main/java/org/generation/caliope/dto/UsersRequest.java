c4ba3c21854679acbd105fcdc075581c53df133c
alejandroblancojmz-droid <alejandro.blanco.jmz@gmail.com>
2026-07-02 11:41:41 -0600

    Delete backend directory

--- backend/caliope/src/main/java/org/generation/caliope/dto/UsersRequest.java
+++
@@ -1,8 +0,0 @@
-package org.generation.caliope.dto;
-
-import java.time.LocalDate;
-
-public record UsersRequest(Long id, String firstName, String lastName, String user_name, String email,
-                           LocalDate birthday, String bio, String picture_avatar, String password,
-                           String created_at ) {
-}
--- backend/caliope/src/main/java/org/generation/caliope/repository/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package org.generation.caliope.repository;
-
-import org.generation.caliope.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Long> {
-}
